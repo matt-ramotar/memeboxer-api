ee9573d61a8d12df1944c3c7296f107d1efede52
Matt Ramotar <mramotar@dropbox.com>
2021-12-15 21:45:34 -0500

    Add routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import RealAuthService from "./api/auth/services/AuthService";
 
 const router = express.Router();
 
-// router.post("/auth/continue/google", new RealAuthService().continueWithGoogle);
-// router.post("/auth/token/validate", new RealAuthService().validateToken);
+router.post("/auth/continue/google", new RealAuthService().continueWithGoogle);
+router.post("/auth/token/validate", new RealAuthService().validateToken);
 
 export default router;
