b03f3a7e90e140b61d08db6475cf67ac978ac3c2
Matt Ramotar <mramotar@dropbox.com>
2021-12-19 18:49:02 -0500

    Add Slack route

---
+++ src/api/slack/services/RealSlackService.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+interface SlackService {
+  handleChallenge(request: Request, response: Response, next: NextFunction): Promise<Response>;
+}
+
+export default class RealSlackService implements SlackService {
+  public async handleChallenge(request: Request, response: Response, _: NextFunction): Promise<Response> {
+    console.log("request", request.body);
+    return response.json({ challenge: request.body.challenge });
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import RealAuthService from "./api/auth/services/AuthService";
+import RealSlackService from "./api/slack/services/RealSlackService";
 import RealStorageService from "./api/storage/services/StorageService";
 
 const router = express.Router();
@@ -8,5 +9,6 @@ router.post("/auth/continue/google", new RealAuthService().continueWithGoogle);
 router.post("/auth/token/validate", new RealAuthService().validateToken);
 router.post("/storage/png", new RealStorageService().uploadPng);
 router.get("/storage/:key", new RealStorageService().getSignedUrl);
+router.post("/slack/challenge", new RealSlackService().handleChallenge);
 
 export default router;
